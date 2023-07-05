1a6dfa04740aae55bc4a631107a20001334d3fb7
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-07-05 11:29:45 +0000

    docker part

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 
 ## routes
-
 http://127.0.0.1:8000/api/v1/books/
+
 http://127.0.0.1:8000/api/v1/books/{id}
--- book_api_project/settings.py
+++ book_api_project/settings.py
@@ -78,17 +78,17 @@ WSGI_APPLICATION = 'book_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'password': 'postgres',
-        'HOST':'db',
-        'PORT':5432 ,
+        'NAME': "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
